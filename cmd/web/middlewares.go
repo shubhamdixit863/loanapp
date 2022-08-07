6a995f1586a9e01a6b3f7302a9a774eef1089b9e
shubhamdixit863 <shubhamdixit863@gmail.com>
2022-08-07 23:20:44 +0530

    Added middleware to the form data api

---
+++ cmd/web/middlewares.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func JwtVerify(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var header = r.Header.Get("token")
+
+		json.NewEncoder(w).Encode(r)
+		header = strings.TrimSpace(header)
+
+		if header == "" {
+			w.WriteHeader(http.StatusForbidden)
+			json.NewEncoder(w).Encode("Missing auth token")
+			return
+		} else {
+			json.NewEncoder(w).Encode(fmt.Sprintf("Token found. Value %s", header))
+		}
+		next.ServeHTTP(w, r)
+	})
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,27 +3,42 @@ package main
 import (
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"net/http"
+	"os"
 )
+
+func init() {
+
+	godotenv.Load()
+
+}
 
 func (app *App) registerroutes() *mux.Router {
 	r := mux.NewRouter()
+	var specurl string = "/swagger.yaml"
 
-	r.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
+	if os.Getenv("ENV") == "local" {
+		specurl = "/swagger.local.yaml"
+	}
+
+	r.Handle(specurl, http.FileServer(http.Dir("./")))
 	// documentation for developers
-	opts := middleware.SwaggerUIOpts{SpecURL: "/swagger.yaml"}
+	opts := middleware.SwaggerUIOpts{SpecURL: specurl}
 	sh := middleware.SwaggerUI(opts, nil)
 	r.Handle("/docs", sh)
 
 	// documentation for share
-	opts1 := middleware.RedocOpts{SpecURL: "/swagger.yaml", Path: "docs1"}
+	opts1 := middleware.RedocOpts{SpecURL: specurl, Path: "docs1"}
 	sh1 := middleware.Redoc(opts1, nil)
 	r.Handle("/docs1", sh1)
 
 	r.HandleFunc("/v1/health", app.HealthcheckHandler).Methods("GET")
 	r.HandleFunc("/v1/login", app.LoginHAndler).Methods("POST")
 	r.HandleFunc("/v1/signup", app.SignupHandler).Methods("POST")
-	r.HandleFunc("/v1/formdata", app.UploadUserFormData).Methods("POST")
+	formdata := r.PathPrefix("/v1/verified").Subrouter()
+	formdata.Use(JwtVerify)
+	formdata.HandleFunc("/formdata", app.UploadUserFormData).Methods("POST")
 
 	return r
 
